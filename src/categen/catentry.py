846ee2510d572436be2e0ae5ebc64cb1220b8988
--global <--global>
2020-12-30 22:39:44 +0800

    Revamp everything

---
+++ src/categen/catentry.py
@@ -0,0 +1,284 @@
+from PrintingPress import Placements, operate
+from hentai import Hentai, Format
+from datetime import datetime
+from copy import deepcopy
+from pathlib import Path
+from PIL import Image
+from json import load
+
+from . import data
+
+
+class Utils:
+    def sanitize(name) -> str:
+        # TODO: This goes up to 2 nested [tags (long)], if the edge
+        # case becomes existant - remake this.
+
+        # Really old [] removal from InterestingSystems/Author,
+        # since nhentai doesnt give pretty Japanese titles.
+
+        stop = [")", "]"]
+        watching = False
+        detagged = name
+        keyword = ""
+        signal = ""
+
+        for letter in name:
+            if watching:
+                keyword = keyword + letter
+
+            if letter == "[" and signal == "":
+                signal = "]"
+                watching = True
+                keyword = letter
+
+            if letter == "(" and signal == "":
+                signal = ")"
+                watching = True
+                keyword = letter
+
+            if letter in stop and letter == signal:
+                watching = False
+                signal = ""
+                detagged = detagged.replace(keyword, "")
+                keyword = ""
+
+        detagged = detagged.split(" ")
+        pure = [elem for elem in detagged if elem != ""]
+
+        return " ".join(pure)
+
+
+class CatalogueEntry:
+    markdown_map = {
+        "WhatsApp": {
+            "bold": ["*", "*"],
+            "italic": ["_", "_"],
+            "scode": ["```", "```"],
+            "mcode": ["```", "```"],
+        },
+        "Telegram": {
+            "bold": ["**", "**"],
+            "italic": ["__", "__"],
+            "scode": ["`", "`"],
+            "mcode": ["```\n", "\n```"],
+        },
+    }
+
+    def __init__(
+        self, eid: int, hid: int, score: str, desc: str = "", markdown: str = "Telegram"
+    ) -> None:
+        self.config = data.Config()
+
+        if not Path(self.config["Repository"]["path"]).is_dir():
+            print(
+                f'categen: {self.config["Repository"]["path"]} is non-existant, '
+                "attempting to download Format."
+            )
+            try:
+                data.Format.retrieve_online(
+                    url=self.config["Repository"]["format_repository"],
+                    path=self.config["Repository"]["path"],
+                )
+
+            except Exception as e:
+                print(
+                    "categen: Error encountered while attempting to download format. "
+                    f"({e}) categen will use the local Format shipped with the package."
+                )
+                data.Format.retrieve_local(format_dir=self.config["Repository"]["path"])
+
+        self.format = data.Format(repo_config=self.config["Repository"])
+        self.doujin = Hentai(hid)
+
+        self.markdown = self.markdown_map[markdown]
+        self.id = "0" * (3 - len(str(eid))) + str(eid)
+        self.score = str(score)
+        self.description = str(desc)
+
+        placements_path = Path(self.config["Repository"]["path"]).joinpath(
+            "placements.json"
+        )
+        assert placements_path.exists(), f"{placements_path}: Non-existant"
+        with open(placements_path, "r", encoding="utf-8") as pf:
+            self.placements = load(pf)
+
+        entry_path = Path(self.config["Repository"]["path"]).joinpath("entry.txt")
+        assert entry_path.exists(), f"{entry_path}: Non-existant"
+        with open(entry_path, "r", encoding="utf-8") as ef:
+            self.entry_text = ef.read()
+
+        tagsofinterest_path = Path(self.config["Repository"]["path"]).joinpath(
+            "tagsofinterest.json"
+        )
+        assert tagsofinterest_path.exists(), f"{tagsofinterest_path}: Non-existant"
+        with open(tagsofinterest_path, "r", encoding="utf-8") as toif:
+            self.tagsofinterest = load(toif)
+
+        formatting_map_path = Path(self.config["Repository"]["path"]).joinpath(
+            "formatting.json"
+        )
+        if formatting_map_path.is_file():
+            with open(formatting_map_path, "r", encoding="utf-8") as fmf:
+                self.formatting_map = load(fmf)
+        else:
+            self.formatting_map = {}
+
+        template_path = Path(self.config["Repository"]["path"]).joinpath(
+            "thumbnail/template.png"
+        )
+        assert template_path.is_file(), f"{template_path}: Non-existant"
+        self.template = Image.open(str(template_path))
+
+        self._thumbnail = None
+        self._entry = None
+
+    def _divide(self, text: str) -> str:
+        if "|" in text and self._divide_type != "both":
+            if self._divide_type == "left":
+                return text.split("|")[0].lstrip().rstrip()
+            else:
+                return text.split("|")[1].lstrip().rstrip()
+
+        else:
+            return text
+
+    def _strf(self, text: str, markdown_type: str = None) -> str:
+        def fword(keyword: str) -> str:
+            override = self.formatting_map.get(keyword)
+            return keyword if override is None else f"{keyword}:{override}"
+
+        if markdown_type is None:
+            markdown_type = self.config["Settings"]["markdown_type"]
+
+        # Title Generation
+        title_p_en = self.doujin.title(Format.Pretty)
+        title_p_jp = Utils.sanitize(self.doujin.title(Format.Japanese))
+
+        markdown = self.markdown_map[markdown_type]
+
+        bold = markdown["bold"][0]
+        _bold = markdown["bold"][1]
+
+        if title_p_en != "" and title_p_jp != "":
+            title = f"{bold}{title_p_en}{_bold} - {title_p_jp}"
+        elif title_p_en != "" and title_p_jp == "":
+            title = f"{bold}{title_p_en}{_bold}"
+        elif title_p_en == "" and title_p_jp != "":
+            title = f"{bold}{title_p_jp}{_bold}"
+        else:
+            title = ""
+
+        # Creator Text Generation
+        artist_names = [tag.name for tag in self.doujin.artist]
+        artist_names_str = ", ".join(artist_names)
+
+        group_names = [tag.name for tag in self.doujin.group]
+        group_names_str = ", ".join(group_names)
+
+        if len(artist_names) > 0 and len(group_names) > 0:
+            creator = f"({artist_names_str} / {group_names_str})"
+        elif len(artist_names) > 0 and len(group_names) == 0:
+            creator = f"({artist_names_str})"
+        elif len(artist_names) == 0 and len(group_names) > 0:
+            creator = f"({group_names_str})"
+        else:
+            creator = ""
+
+        # Tags
+        tags = [self._divide(tag.name) for tag in self.doujin.tag]
+        tags_interest = [tag for tag in tags if tag in self.tagsofinterest]
+        tags_remainder = [tag for tag in tags if tag not in tags_interest]
+
+        format_map = {
+            "bold": markdown["bold"][0],
+            "-bold": markdown["bold"][1],
+            "italic": markdown["italic"][0],
+            "-italic": markdown["italic"][1],
+            "scode": markdown["scode"][0],
+            "-scode": markdown["scode"][1],
+            "mcode": markdown["mcode"][0],
+            "-mcode": markdown["mcode"][1],
+            fword("entry_id"): self.id,
+            fword("rating"): self.score,
+            fword("description"): self.description,
+            fword("title.english"): self.doujin.title(),
+            fword("title.english.pretty"): title_p_en,
+            fword("title.japanese:"): self.doujin.title(Format.Japanese),
+            fword("title.japanese.pretty"): title_p_jp,
+            fword("title"): title,
+            fword("tags.interest"): ", ".join(tags_interest),
+            fword("tags.remainder"): ", ".join(tags_remainder),
+            fword("tags"): ", ".join(tags),
+            fword("creator.scanlator"): self.doujin.scanlator,
+            fword("creator.artist"): ", ".join([t.name for t in self.doujin.artist]),
+            fword("creator.group"): ", ".join([t.name for t in self.doujin.group]),
+            fword("creator"): creator,
+            fword("doujin_id"): self.doujin.id,
+            fword("pages"): self.doujin.num_pages,
+            fword("favourites"): self.doujin.num_favorites,
+            fword("link"): self.doujin.url,
+            fword("time"): datetime.fromtimestamp(self.doujin.epos).strftime(
+                "%B %d %Y, %H:%M:%S"
+            ),
+            fword("parody"): ", ".join([t.name for t in self.doujin.parody]),
+            fword("characters"): ", ".join([t.name for t in self.doujin.character]),
+            fword("slink"): f"nh.{self.doujin.id}",
+        }
+
+        for keyword, replacement in format_map.items():
+            conditions = [
+                "parody" in keyword and replacement == "original",
+                "parody" in keyword and replacement == "",
+                "favourites" in keyword and replacement == "0",
+                "character" in keyword and replacement == "",
+                "creator.group" in keyword and replacement == "",
+                "description" in keyword and replacement == "",
+            ]
+            if not any(conditions):
+                if ":" in keyword:
+                    # keyword:\nkeyword\n
+                    _keyword = keyword
+                    keyword = keyword.split(":")[0]
+                    replacement = _keyword.lstrip(keyword.split(":")[0] + ":").replace(
+                        "{}", replacement
+                    )
+
+                text = self._divide(text.replace(f"<[{keyword}]>", str(replacement)))
+
+            else:
+                text = text.replace(f"<[{keyword}]>", "")
+
+        return text
+
+    def thumbnail(self, preview: Image.Image, suppress: bool = False) -> Image.Image:
+        placements = deepcopy(self.placements)
+
+        if '.meta' in placements:
+            placements.pop('.meta')
+
+        for area in [
+            area for area in placements if placements[area].get("type") == "text"
+        ]:
+            placements[area]["text"] = self._strf(placements[area]["text"])
+
+        for area in [area for area in placements if placements[area]['path'] != '']:
+            placements[area]['path'] = str(Path(self.config['Repository']['path']).joinpath(placements[area]['path']))
+
+        placements["viewfinder"]["path"] = preview
+
+        self._thumbnail = operate(
+            image=self.template,
+            placements=Placements.parse(placements),
+            suppress=suppress,
+        )
+        return self._thumbnail
+
+    def entry(self, markdown_type: str = None) -> str:
+        self._entry = self._strf(self.entry_text)
+        return self._entry
+
+    def export(self) -> bytes:
+        import dill
+
+        return dill.dump(self)
---
+++ src/categen/data.py
@@ -0,0 +1,172 @@
+from os import getenv, mkdir
+from platform import system
+from pathlib import Path
+
+
+_file_dir = Path(__file__).parent
+_local_format_dir = Path(_file_dir.joinpath("format"))
+_data_dir_map = {
+    "Linux": getenv("XDG_DATA_DIR", Path(Path.home(), ".local/share")),
+    "Windows": getenv("XDG_DATA_DIR", Path(Path.home(), "AppData/Roaming")),
+    "macOS": getenv("XDG_DATA_DIR", Path(Path.home(), "Library")),
+}
+
+data_dir = Path(_data_dir_map[system()]).joinpath("interestingimages")
+format_dir = data_dir.joinpath("format")
+config_file = data_dir.joinpath("categen.toml")
+backup_file = data_dir.joinpath("format-ver.tar.bz2")
+
+if not data_dir.is_dir():
+    mkdir(data_dir)
+
+
+class Config:
+    default = {
+        "Settings": {
+            "markdown_type": "Telegram",
+            "divide": "both",
+            "interestingtags": True,
+        },
+        "Repository": {
+            "path": str(format_dir),
+            "format_repository": "git://github.com/interestingimages/Format.git",
+            "latest_placements": "https://raw.githubusercontent.com/"
+            "interestingimages/Format/master/placements.json",
+            "backup_file": str(backup_file),
+        },
+    }
+
+    def __init__(self, encoding: str = "utf-8") -> None:
+        import tomlkit
+
+        self.tomlkit = tomlkit
+
+        if not config_file.is_file():
+            self.file = open(config_file, "w", encoding=encoding)
+
+            # Make new config file
+            self.data = self.tomlkit.document()
+
+            for key, value in Config.default.items():
+                self.data[key] = value
+
+            # Cleanup
+            self.file.write(self.tomlkit.dumps(self.data))
+            self.file.flush()
+
+        else:
+            self.file = open(config_file, "r+", encoding=encoding)
+            self.data = tomlkit.loads(self.file.read())
+
+            # Add unadded tables
+            for table in [
+                t for t in Config.default.keys() if t not in self.data.keys()
+            ]:
+                print(
+                    f'categen: Appending non-present table "{table}" not present '
+                    "in categen.toml."
+                )
+                self.data.update({table: Config.default[table]})
+
+            # Add unadded keys to existing tables
+            for table, contents in self.data.items():
+                if table in Config.default:
+                    if contents.keys() != Config.default[table].keys():
+                        for key in [
+                            k
+                            for k in Config.default[table].keys()
+                            if k not in contents.keys()
+                        ]:
+                            print(
+                                f'categen: Appending non-present key value pair "{key}" '
+                                f"not present in categen.toml:{table}"
+                            )
+                            self.data[table][key] = Config.default[table][key]
+
+            # Cleanup
+            self.file.seek(0)
+            self.file.write(self.tomlkit.dumps(self.data))
+            self.file.flush()
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def get(self, item, default=None):
+        return self.data.get(item, default=default)
+
+    def write(self) -> None:
+        self.file.seek(0)
+        self.write(self.tomlkit.dumps(self.data))
+        self.file.flush()
+
+    def close(self) -> None:
+        self.file.close()
+
+
+class Format:
+    def __init__(self, repo_config: dict) -> None:
+        import git
+
+        self.git = git
+        self.config = repo_config
+        self.repo = self.git.Repo(self.config["path"])
+
+    def pull(self):
+        origin = self.repo.remotes["origin"]
+        origin.pull()
+
+    def backup(self, file_path=None, format_path=None) -> None:
+        from tarfile import open
+
+        if file_path is None:
+            file_path = self.config["backup_file"]
+
+        if format_path is None:
+            format_path = self.config["path"]
+
+        with open(
+            str(file_path).replace("ver", Format.current_ver()), mode="w:bz2"
+        ) as backup:
+            backup.add(format_path)
+
+    def clone(self, path=None, url=None) -> None:
+        if url is None:
+            url = self.config["format_repository"]
+
+        if path is None:
+            path = self.config["path"]
+
+        assert any(Path(path).iterdir()), f"{str(path)} is not empty."
+
+        repo = self.git.Repo.clone_from(url=url, to_path=path)
+
+        self.repo = repo
+
+    @staticmethod
+    def current_ver() -> str:
+        from json import load
+
+        placements = open(format_dir.joinpath("placements.json"), "r", encoding="utf-8")
+        current = load(placements)[".meta"]["version"]
+        placements.close()
+
+        return current
+
+    @staticmethod
+    def latest_ver(url: str) -> str:
+        from requests import get
+        from json import loads
+
+        return loads(get(url).text)[".meta"][".version"]
+
+    @staticmethod
+    def retrieve_local(format_dir=format_dir) -> None:
+        from shutil import move
+
+        move(_local_format_dir, format_dir)
+
+    @staticmethod
+    def retrieve_online(url, path) -> None:
+        from git import Repo
+
+        Repo.clone_from(url=url, to_path=path)
